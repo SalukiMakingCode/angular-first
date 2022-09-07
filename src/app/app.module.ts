39b86d446fa91689cb05038c977353356b6ab8d6
David Van Goidtsnoven <vangoidtsnovendavid@gmail.com>
2022-09-07 15:29:40 +0200

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloAngularComponent } from './hello-angular/hello-angular.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { Demo1Component } from './demos/demo1/demo1.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloAngularComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    Demo1Component,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
